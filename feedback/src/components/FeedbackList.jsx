cbb7fb5b58541789920d147fcf08efa0b91bd618
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-09-07 14:51:26 -0700

    framer-motion and uuid packages added

--- feedback/src/components/FeedbackList.jsx
+++ feedback/src/components/FeedbackList.jsx
@@ -1,19 +1,33 @@
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function FeedbackList(props) {
   return (
     <div className="feedback-list">
-      {props.feedback.map((f) => {
-        return (
-          <FeedbackItem key={f.id} item={f} handleDelete={props.handleDelete} />
-        );
-      })}
+      <AnimatePresence>
+        {props.feedback.map((f) => {
+          return (
+            <motion.div
+              key={f.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={f.id}
+                item={f}
+                handleDelete={props.handleDelete}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
 }
 
 FeedbackList.propTypes = {
-  feedback: PropTypes.object.isRequired,
+  feedback: PropTypes.array.isRequired,
 };
 export default FeedbackList;
