b6ce98bbfd369388c2d8a8a323c3e332e8580cf6
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-09-07 14:57:27 -0700

    update add new feedbacks

--- feedback/src/App.jsx
+++ feedback/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 
@@ -10,11 +13,17 @@ function App() {
     if (window.confirm('Are you sure you want to delete?'))
       setFeedback(feedback.filter((item) => item.id !== id));
   };
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
