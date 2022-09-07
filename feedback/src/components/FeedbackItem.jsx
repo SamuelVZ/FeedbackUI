f3f1be18e5bb855736d4545e18552e60e85b40e1
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-09-07 08:46:00 -0700

    delete button added, updates the sate

--- feedback/src/components/FeedbackItem.jsx
+++ feedback/src/components/FeedbackItem.jsx
@@ -1,9 +1,23 @@
+import Card from './shared/Card';
+import PropTypes from 'prop-types';
+import { MdDeleteForever } from 'react-icons/md';
+
 function FeedbackItem(props) {
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{props.item.rating}</div>
+      <button
+        className="close"
+        onClick={() => props.handleDelete(props.item.id)}
+      >
+        <MdDeleteForever color="red" />
+      </button>
       <div className="text-display">{props.item.text}</div>
-    </div>
+    </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 export default FeedbackItem;
--- feedback/src/components/FeedbackList.jsx
+++ feedback/src/components/FeedbackList.jsx
@@ -1,12 +1,19 @@
 import FeedbackItem from './FeedbackItem';
+import PropTypes from 'prop-types';
 
 function FeedbackList(props) {
   return (
     <div className="feedback-list">
       {props.feedback.map((f) => {
-        return <FeedbackItem key={f.id} item={f} />;
+        return (
+          <FeedbackItem key={f.id} item={f} handleDelete={props.handleDelete} />
+        );
       })}
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.object.isRequired,
+};
 export default FeedbackList;
